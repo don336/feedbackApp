317bb71f8504e907b88f50187047f150fa5f5000
Ssemugabi Martin <76908155+dombreaker36@users.noreply.github.com>
2022-10-03 16:19:50 +0300

    Added a mock database

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -1,22 +1,24 @@
 import React, { useContext } from 'react'
 import FeedbackItem from './FeedbackItem'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
 
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext)
-  if(!feedback || feedback.length === 0){
+  const { feedback, isLoading } = useContext(FeedbackContext)
+  if(!isLoading && (!feedback || feedback.length === 0)){
+
     return <p>No Feedback returned</p>
   }
-  return ( 
-    <div className='feedback-List'>
-        {feedback.map((item)=>(
-         <FeedbackItem key={item.id} item={item} />
-        // <div>{item.rating}</div>
-       ))}
 
+  return isLoading ? <Spinner /> : (
+    <div className='feedback-List'>
+    {feedback.map((item)=>(
+     <FeedbackItem key={item.id} item={item} />
+    // <div>{item.rating}</div>
+   ))}
     </div>
-  )
+  ) 
 
 
 }
